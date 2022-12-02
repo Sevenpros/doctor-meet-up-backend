ca60e71e42a6f57c8ca37cfd696b0e2eba735151
Anas Abubakar <anasabubakar2015@gmail.com>
2022-12-02 12:41:21 +0100

    Add functionalities to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :doctors
-  resources :users do
-    resources :bookings, only: [:index, :show, :create, :destroy]
+  resources :doctors only: [:new, :create, :index, :show]
+  resources :users only: [:new, :create] do
+    resources :bookings, only: [:new, :index, :show, :create, :destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
