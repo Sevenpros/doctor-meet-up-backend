d8610c81ad5fd63f0627d08052ae2e89260dc623
Sevenpros <sethbisoso@gmail.com>
2022-12-02 11:26:10 +0100

    Add basic serup files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Doctors first_name last_name specilization phone email city bio photo
+Booking datetime city doctor_id user_id disease_desc
+User name email 
