9a49a26b5661c0d64422943a08daa1b1034a5416
Sevenpros <sethbisoso@gmail.com>
2022-12-09 14:56:04 +0100

    Fix port issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,12 @@ class UsersController < ApplicationController
   end
 
   # POST /users
+
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
